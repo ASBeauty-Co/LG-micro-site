fbb07ee884b80dc5aa7669a452705a57eff63ae1
iceesocial <contact@iceesocial.com>
2026-04-10 14:03:49 -0400

    blog update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ var swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 2.5,
 });
+
+// Smooth scrolling for anchor links + hidden anchor id in the url
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+          targetElement.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
