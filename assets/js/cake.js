6c1073622ab170a36d52de97e4ac20b5726aac0c
iceesocial <contact@iceesocial.com>
2026-04-22 12:46:04 -0400

    cake update

--- assets/js/cake.js
+++ assets/js/cake.js
@@ -1,8 +1,8 @@
-const cookieDomain = 'alchemyworx.cakeengine.net';
-const siteDomain = '.laurageller.online';
-
 // DCP Click
 document.addEventListener('DOMContentLoaded', () => {
+    const cookieDomain = 'alchemyworx.cakeengine.net';
+    const siteDomain = '.laurageller.online';
+    
    (function ()  {
         var c = { 'name': 'cp', 'args': {'sess_timeout':'','gclid_param':'gclid','gclid_chk_ref':0,'bingkw_param':'ckm_kw','sc_param':'ckmsc','bingkw_chk_ref':0, 'attr_param':'ckmscn', 'attr_chk_ref': 0, 'sess_param': '', 'sess_chk_ref': 0, 'crtv_id': null, 'crtv_param':'ckmc', 'crtv_chk_ref': 0, 'tpcrid_param': 'ckmcrn', 'cookie_dom': cookieDomain, 'site_dom': siteDomain, 'ref_type': 1, 'ckm_sess_param': null ,'s1_param':'s1','s2_param':'s2','s3_param':'s3','s4_param':'s4','s5_param':'s5', 'sub_chk_ref':0} };
         var params = new URLSearchParams(window.location.search);
