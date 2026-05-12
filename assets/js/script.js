ba021e2eca6dd5b0fabc8e06ccf752092c0db5ab
ICEE Social <60201557+iceesocial@users.noreply.github.com>
2026-05-12 16:22:42 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,62 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       }
   });
 });
+
+//Blotout Pixel FB ICEE 05-12-26
+  const edgeURL = 'https://dsscv.laurageller.online';
+
+  !(function (u) {
+    window.edgetag =
+      window.edgetag ||
+      function () {
+        (edgetag.stubs = edgetag.stubs || []).push(arguments);
+      };
+    const t = document.createElement('script');
+    (t.type = 'text/javascript'), (t.src = u), (t.async = !0);
+    const e = document.getElementsByTagName('script')[0];
+    e.parentNode.insertBefore(t, e);
+  })(`${edgeURL}/load`);
+  !(function () {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    const userId = params.get('et_u_id') || '';
+    edgetag('init', {
+      edgeURL,
+      disableConsentCheck: true,
+      userId,
+    });
+  })();
+
+  edgetag('tag', 'PageView', {}, {}, { destination: edgeURL });
+
+
+  (function () {
+    const modifyUrls = (userId) => {
+      const links = document.querySelectorAll('a[href]');
+
+      links.forEach((link) => {
+        const href = link.href;
+        if (
+          !href.startsWith('http') &&
+          !href.startsWith('/') &&
+          !href.startsWith('#')
+        ) {
+          return;
+        }
+
+        try {
+          const url = new URL(href, window.location.origin);
+          url.searchParams.set('et_u_id', userId);
+          link.href = url.toString();
+        } catch {}
+      });
+    };
+
+    window.addEventListener('edgetag-initialized', ({ detail }) => {
+      if (!detail.userId) {
+        return;
+      }
+
+      modifyUrls(detail.userId);
+    });
+  })();
